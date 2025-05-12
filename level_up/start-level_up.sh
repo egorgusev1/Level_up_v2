0339dcb904281282761beab33566c01344652c17
Egor Gusev <egor.gusev2018@gmail.com>
2025-05-12 01:11:29 -0400

    trying fix for wsgi file

--- level_up/start-level_up.sh
+++ level_up/start-level_up.sh
@@ -8,7 +8,7 @@ poetry run python manage.py migrate
 GUNICORN_WORKERS=${GUNICORN_WORKERS:-3}
 
 if [[ "$ENV_STATE" == "production" ]]; then
-    poetry run gunicorn level_up.wsgi. --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
+    poetry run gunicorn level_up/level_up.wsgi. --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
 else
     poetry run python manage.py runserver 0.0.0.0:8000
 fi
