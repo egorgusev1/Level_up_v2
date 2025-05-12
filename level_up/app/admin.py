ea13f4617f0381559b5560c883b12d48c9928fbe
Egor Gusev <egor.gusev2018@gmail.com>
2025-05-11 23:30:08 -0400

    removed 2nd password verification, added verification for email for superuser

--- level_up/app/admin.py
+++ level_up/app/admin.py
@@ -35,7 +35,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("email","password1","password2")
+            "fields": ("email","password1")
             }),
     )
     list_display = ("email","is_staff","is_active")
