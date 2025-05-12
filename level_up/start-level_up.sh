78e2d9ca0ce2b3ddbc6bf9126d8f16ed48f64624
Egor Gusev <egor.gusev2018@gmail.com>
2025-05-12 01:14:01 -0400

    changed the start-level_up.sh

--- level_up/start-level_up.sh
+++ level_up/start-level_up.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-
+# Run collectstatic and migrations
 poetry run python manage.py collectstatic --no-input
-
 poetry run python manage.py migrate
 
+# Set default number of Gunicorn workers if not provided
 GUNICORN_WORKERS=${GUNICORN_WORKERS:-3}
 
 if [[ "$ENV_STATE" == "production" ]]; then
-    poetry run gunicorn level_up/level_up.wsgi. --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
+    # Corrected WSGI path: assuming wsgi.py is in level_up/level_up/
+    poetry run gunicorn level_up.wsgi:application \
+        --workers "$GUNICORN_WORKERS" \
+        --bind 0.0.0.0:8000 \
+        --forwarded-allow-ips "*"
 else
     poetry run python manage.py runserver 0.0.0.0:8000
 fi
-
