bce93a4bf379c12f1523169372eecaee3793d109
Egor Gusev <egor.gusev2018@gmail.com>
2025-05-12 01:01:42 -0400

    working on a fix for deployment

--- level_up/start-level_up.sh
+++ level_up/start-level_up.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+npx tailwindcss -i ./static/input.css -o ./static/output.css --minify
+
 poetry run python manage.py collectstatic --no-input
+
 poetry run python manage.py migrate
 
+GUNICORN_WORKERS=${GUNICORN_WORKERS:-3}
+
 if [[ "$ENV_STATE" == "production" ]]; then
-    poetry run gunicorn level_up.wsgi. --workers $GUNICORN_WORKERS --forwared-allow-ips "*"
+    poetry run gunicorn level_up.wsgi. --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
 else
     poetry run python manage.py runserver 0.0.0.0:8000
 fi
