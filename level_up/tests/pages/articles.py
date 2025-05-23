2409a61acec4c33c09aa8b2818a8294b7fc0da1f
Egor Gusev <egor.gusev2018@gmail.com>
2025-05-23 11:15:42 -0400

    added tests for creating and editing articles

--- level_up/tests/pages/articles.py
+++ level_up/tests/pages/articles.py
@@ -12,4 +12,33 @@ class ArticlesPage:
         #text elements
         self.welcome_heading = page.get_by_role("heading", name="Welcome")
         self.no_articles_message = page.get_by_text("You have no articles yet")
+
+
+class ArticlePage:
+    def __init__(self,page: Page) -> None:
+        self.page = page
+        self.title_input = page.get_by_placeholder("Write a title for your article")
+        self.status_select = page.locator("#id_status")
+        self.content_editor = page.locator("#id_content")
+        self.code_mirror_editor = page.locator(".CodeMirror-scroll")
+        self.twitter_post_input = page.locator("#id_twitter_post")
+
+        self.save_button = page.get_by_role("button", name="Save")
+
+    def set_status(self,status: str):
+        self.status_select.select_option(status)
+
+    
+    def fill_article_content(self, content: str):
+        self.code_mirror_editor.click()
+        self.page.keyboard.insert_text(content)
+
+    def fill_article(
+            self, title: str, content: str, twitter_post: str = "", status: str = "draft"
+    ):
+        self.title_input.fill(title)
+        self.set_status(status)
+        self.fill_article_content(content)
+        self.twitter_post_input.fill(twitter_post)
+        
         
